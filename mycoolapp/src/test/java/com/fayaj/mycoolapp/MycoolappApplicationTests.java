5bc40712372f5e871974c709cd770f817ef82c71
shaman <fayazhossain88@gmail.com>
2023-07-17 13:52:58 +0530

    learning spring

---
+++ mycoolapp/src/test/java/com/fayaj/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fayaj.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
