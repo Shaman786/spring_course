de66878e5d67024bd3211861a692c5af0b7a7c6a
Fayaj Hossain <fayazhossain88@gmail.com>
2023-08-27 11:11:38 +0530

    working spring web and pojos

---
+++ 04-Spring-Boot-Rest-CRUD/01-spring-boot-rest-crud/src/test/java/com/shaman/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaman.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
