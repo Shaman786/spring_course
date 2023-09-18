ddf60a6ab87091ad5e4dca63872fb6c4e716138c
Fayaj Hossain <fayazhossain88@gmail.com>
2023-09-18 19:41:44 +0530

    rest-crud-employee

--- 04-Spring-Boot-Rest-CRUD/01-spring-boot-rest-crud/src/main/java/com/shaman/demo/rest/StudentRestController.java
+++ 04-Spring-Boot-Rest-CRUD/01-spring-boot-rest-crud/src/main/java/com/shaman/demo/rest/StudentRestController.java
@@ -47,17 +47,7 @@ public class StudentRestController {
     }
 
     //    Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-//        create a Student Error Response
-        StudentErrorResponse error = new StudentErrorResponse();
 
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-//        return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
 }
 
 
