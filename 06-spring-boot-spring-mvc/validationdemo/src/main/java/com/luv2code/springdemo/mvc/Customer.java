42518067ba9904f6c69a19954a5e0761321143e6
Fayaj Hossain <fayazhossain88@gmail.com>
2023-10-11 01:00:46 +0530

    mvc-commit

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -20,6 +21,17 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    @CourseCode
+    private String courseCode;
 
     public String getPostalCode() {
         return postalCode;
